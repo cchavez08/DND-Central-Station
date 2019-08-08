0365fc1669c370ae5eec2d114dec62a5cc1c1dff
Cristian Chavez <cchavez082002@gmail.com>
2019-08-07 17:51:57 -0700

    Update

--- main.js
+++ main.js
@@ -31,7 +31,12 @@ function getParameterByName(name, url) {
         var html = [];
         var name = record.fields['Name'];
         var details = record.fields['Details'];
-        html.push(detailView(name, details, id));
+        var photos = record.fields['Photos'];
+        var modetails = record.fields['MoreDetails'];
+        var ccp3 = record.fields['CCP3'];
+        var ccp4 = record.fields['CCP4'];
+        var ccp5 = record.fields['CCP5'];
+        html.push(detailView(name, details, photos, modetails, ccp3, ccp4, ccp5, id));
         $('.detail-view').append(html);
       }
       );
@@ -40,21 +45,32 @@ function getParameterByName(name, url) {
   
   var listView = function(id, name, description, pic) {
   return `
-  <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-  <div class="card-body">
-  <h2 class="card-title"><a href="index.html?id=${id}">${name}</a> </h2>
-  <p class="card-text">${description}</p>
-  </div>
-    ${pic?`<img src="${pic[0].url}">` : ``}
+  <div class="card bg-dark mb-3" style="max-width: 18rem;">
+    <div class="card-body">
+      <h2 class="card-title"><a href="index.html?id=${id}">${name}</a> </h2>
+      <p class="card-text">${description}</p>
+    </div>
+  ${pic?`<img src="${pic[0].url}">` : ``}
   </div>
   `;
   }
   
 
-  var detailView = function(name, details) {
+  var detailView = function(name, details, photos, modetails, ccp3, ccp4, ccp5) {
     return `
-    <h1>${name}</h1>
-    <p>${details}</p>
+    <div id="dv" class="card bg-dark mb-3" style="max-width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${details}</p>
+        <p class="2nd-card-text">${modetails}</p>
+        <p class="3rd-card-text">${ccp3}</p>
+        <p class="4th-card-text">${ccp4}</p>
+        <p class="5th-card-text">${ccp5}</p>
+      </div>
+      <div class="pictures">
+      ${photos?`<img src="${photos[0].url}">` : ``}
+      </div>
+    </div>
     `;
   }
   
